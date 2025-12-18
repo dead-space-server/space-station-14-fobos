ee3f038eaf66290ad0c1d030a47a1e87e0e045e9
KloopRE <100780612+KloopRE@users.noreply.github.com>
2025-12-18 13:51:06 +0300

    исправления

--- Content.Server/DeadSpace/Virus/Components/SpawnAntagAfterSelectedComponent.cs
+++ Content.Server/DeadSpace/Virus/Components/SpawnAntagAfterSelectedComponent.cs
@@ -1,12 +1,12 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
-using Content.Shared.Roles.Components;
+using Content.Server.DeadSpace.Virus.Systems;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server.DeadSpace.Virus.Components;
 
-[RegisterComponent]
-public sealed partial class SpawnAntagAfterSelectedComponent : BaseMindRoleComponent
+[RegisterComponent, Access(typeof(SpawnAntagAfterSelectedRule))]
+public sealed partial class SpawnAntagAfterSelectedComponent : Component
 {
     [DataField]
     [ViewVariables(VVAccess.ReadOnly)]
