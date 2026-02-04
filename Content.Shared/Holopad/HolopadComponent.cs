b50dd384c5ee2fd0e294d3e61ab63b81bd10bcd7
TheZuTi <Daniil9426@gmail.com>
2026-02-04 02:09:20 +0200

    ss

--- Content.Shared/Holopad/HolopadComponent.cs
+++ Content.Shared/Holopad/HolopadComponent.cs
@@ -56,14 +56,6 @@ public sealed partial class HolopadComponent : Component
     /// </summary>
     [DataField]
     public float ControlLockoutCoolDown { get; private set; } = 180f;
-
-    //DS14-start
-    [DataField, ViewVariables, AutoNetworkedField]
-    public bool Portable { get; set; }
-
-    [DataField, ViewVariables, AutoNetworkedField]
-    public bool Deployed { get; set; }
-    //DS14-end
 }
 
 #region: Event messages
