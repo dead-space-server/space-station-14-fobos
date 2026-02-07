6e3ceae5d7214bff5d11f8d893d46099677cc621
TheZuTi <Daniil9426@gmail.com>
2026-02-07 19:34:13 +0200

    ііі

---
+++ Content.Shared/DeadSpace/Movement/Component/SpeedSlidingComponent.cs
@@ -0,0 +1,25 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.DeadSpace.Abilities.Slide;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SpeedSlidingComponent : Component
+{
+    [DataField] 
+    public float MinSlideSpeed = 4.9f;
+
+    [DataField] 
+    public float SlideDistance = 6.5f;
+
+    [DataField] 
+    public float SlideSpeed = 3.5f;
+
+    [DataField] 
+    public SoundSpecifier? SlideSound;
+
+    [ViewVariables]
+    public bool IsSliding = false;
+}
