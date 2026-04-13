19bc70cf97f818ef924e973717065cbcf57a6fe2
ZweiL <damirstorozhuk@gmail.com>
2026-04-13 23:14:12 +0300

    fixiki

--- Content.Shared/DeadSpace/NicotineAddiction/NicotineAddictionEffect.cs
+++ Content.Shared/DeadSpace/NicotineAddiction/NicotineAddictionEffect.cs
@@ -1,30 +1,11 @@
 using Content.Shared.EntityEffects;
-using Content.Shared.StatusEffectNew;
-using Robust.Shared.IoC;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
 
 namespace Content.Shared.DeadSpace.NicotineAddiction;
 
-public sealed partial class NicotineAddictionEffect : EntityEffect
+public sealed partial class NicotineAddictionEffect : EntityEffectBase<NicotineAddictionEffect>
 {
-    public override void Effect(EntityEffectBaseArgs args)
-    {
-        if (!args.EntityManager.TryGetComponent<NicotineAddictionComponent>(args.TargetEntity, out var comp))
-            return;
-
-        var timing = IoCManager.Resolve<IGameTiming>();
-        comp.LastNicotineInBloodTime = timing.CurTime;
-        comp.DeprivationPopupShown = false;
-        comp.DeprivationPopupShownAt = TimeSpan.Zero;
-        if (!comp.DeprivationShakeActive)
-            return;
-
-        comp.DeprivationShakeActive = false;
-        args.EntityManager.Dirty(args.TargetEntity, comp);
-    }
-
-    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+    public override string? EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
     {
         return null;
     }
