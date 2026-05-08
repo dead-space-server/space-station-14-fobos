c372d1341b70416c058c49d9a6af83be27dd5d9c
Алексей Герасимов <gerasimovav@althub.pro>
2026-05-08 18:25:39 +0300

    я перфекционист...

--- Content.Shared/DeadSpace/Carrying/SharedCarryInteractionSystem.cs
+++ Content.Shared/DeadSpace/Carrying/SharedCarryInteractionSystem.cs
@@ -1,4 +1,5 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
 using Content.Shared.Body.Systems;
 using Content.Shared.DragDrop;
 using Content.Shared.Interaction.Events;
