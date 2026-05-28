9ebcec2ca0625413e4adc6ee48fa9cbbb88a76d4
Tea2283 <artembasov148@gmail.com>
2026-05-28 22:31:43 +0500

    adasdad

--- Content.Server/Dragon/DragonSystem.cs
+++ Content.Server/Dragon/DragonSystem.cs
@@ -10,6 +10,7 @@ using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Systems;
 using Content.Shared.NPC.Systems;
+using Content.Shared.DeadSpace.Necromorphs.InfectionDead.Components; //DS14
 using Content.Shared.Zombies;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Map.Components;
@@ -69,13 +70,18 @@ public sealed partial class DragonSystem : EntitySystem
             {
                 comp.WeakenedAccumulator -= frameTime;
 
-                // No longer weakened.
+                    // No longer weakened.
                 if (comp.WeakenedAccumulator < 0f)
                 {
                     comp.WeakenedAccumulator = 0f;
                     _movement.RefreshMovementSpeedModifiers(uid);
                 }
             }
+
+            // DS14-Start | Дракон не помирает от времени если заражен
+            if (HasComp<ZombieComponent>(uid) || HasComp<NecromorfComponent>(uid))
+                continue;
+            //DS14-End
 
             // At max rifts
             if (comp.Rifts.Count >= RiftsAllowed)
@@ -123,6 +129,14 @@ public sealed partial class DragonSystem : EntitySystem
             _popup.PopupEntity(Loc.GetString("carp-rift-weakened"), uid, uid);
             return;
         }
+        //DS14-Start
+
+        if (HasComp<ZombieComponent>(uid) || HasComp<NecromorfComponent>(uid))
+        {
+            _popup.PopupEntity(Loc.GetString("carp-rift-infected"), uid, uid);
+            return;
+        }
+        //DS14-End
 
         if (component.Rifts.Count >= RiftsAllowed)
         {
