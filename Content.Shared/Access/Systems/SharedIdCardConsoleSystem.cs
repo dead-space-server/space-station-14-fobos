b70457d68eb890298979cdd9bd92533c8115d5b0
Tea2283 <artembasov148@gmail.com>
2026-06-03 08:49:11 +0500

    последний тест

--- Content.Shared/Access/Systems/SharedIdCardConsoleSystem.cs
+++ Content.Shared/Access/Systems/SharedIdCardConsoleSystem.cs
@@ -24,13 +24,12 @@ namespace Content.Shared.Access.Systems
             SubscribeLocalEvent<IdCardConsoleComponent, ComponentInit>(OnComponentInit);
             SubscribeLocalEvent<IdCardConsoleComponent, ComponentRemove>(OnComponentRemove);
         }
-    
+
         private void OnComponentInit(EntityUid uid, IdCardConsoleComponent component, ComponentInit args)
         {
             _itemSlotsSystem.AddItemSlot(uid, IdCardConsoleComponent.PrivilegedIdCardSlotId, component.PrivilegedIdSlot);
             _itemSlotsSystem.AddItemSlot(uid, IdCardConsoleComponent.TargetIdCardSlotId, component.TargetIdSlot);
         }
-
 
         private void OnComponentRemove(EntityUid uid, IdCardConsoleComponent component, ComponentRemove args)
         {
