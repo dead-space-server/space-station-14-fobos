614c15891c67c6151cb9880e4040b486f0fd0ed9
adasd <adssdasd@gmail.com>
2026-06-17 23:19:34 +0500

    ремап дельты

--- Content.Server/DeadSpace/Borgs/BorgRideSystem.cs
+++ Content.Server/DeadSpace/Borgs/BorgRideSystem.cs
@@ -1,5 +1,7 @@
+using Content.Shared.Buckle;
 using Content.Shared.Buckle.Components;
 using Content.Shared.DeadSpace.Borgs;
+using Content.Shared.Hands;
 using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction.Events;
@@ -10,15 +12,24 @@ namespace Content.Server.DeadSpace.Borgs;
 
 public sealed class BorgRideSystem : EntitySystem
 {
+    [Dependency] private readonly SharedBuckleSystem _buckle = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtual = default!;
 
     public override void Initialize()
     {
+        SubscribeLocalEvent<BorgRideComponent, StrapAttemptEvent>(OnBorgStrapAttempt);
         SubscribeLocalEvent<BorgRideComponent, StrappedEvent>(OnBorgStrapped);
         SubscribeLocalEvent<BorgRideComponent, UnstrappedEvent>(OnBorgUnstrapped);
+        SubscribeLocalEvent<BorgRideComponent, VirtualItemDeletedEvent>(OnBorgVirtualItemDeleted);
         SubscribeLocalEvent<BorgRiderComponent, PickupAttemptEvent>(OnRiderPickupAttempt);
         SubscribeLocalEvent<BorgRiderComponent, AttackAttemptEvent>(OnRiderAttackAttempt);
+    }
+
+    private void OnBorgStrapAttempt(Entity<BorgRideComponent> ent, ref StrapAttemptEvent args)
+    {
+        if (!HasComp<HandsComponent>(args.Buckle.Owner))
+            args.Cancelled = true;
     }
 
     private void OnBorgStrapped(Entity<BorgRideComponent> ent, ref StrappedEvent args)
@@ -26,7 +37,10 @@ public sealed class BorgRideSystem : EntitySystem
         var rider = args.Buckle.Owner;
 
         if (!HasComp<HandsComponent>(rider))
+        {
+            _buckle.TryUnbuckle(rider, rider);
             return;
+        }
 
         foreach (var hand in _hands.EnumerateHands(rider))
         {
@@ -35,11 +49,15 @@ public sealed class BorgRideSystem : EntitySystem
         }
 
         if (!_virtual.TrySpawnVirtualItemInHand(ent.Owner, rider, out _))
+        {
+            _buckle.TryUnbuckle(rider, rider);
             return;
+        }
 
         if (!_virtual.TrySpawnVirtualItemInHand(ent.Owner, rider, out _))
         {
             _virtual.DeleteInHandsMatching(rider, ent.Owner);
+            _buckle.TryUnbuckle(rider, rider);
             return;
         }
 
@@ -53,6 +71,14 @@ public sealed class BorgRideSystem : EntitySystem
         _virtual.DeleteInHandsMatching(rider, ent.Owner);
 
         RemComp<BorgRiderComponent>(rider);
+    }
+
+    private void OnBorgVirtualItemDeleted(Entity<BorgRideComponent> ent, ref VirtualItemDeletedEvent args)
+    {
+        if (!TryComp<BuckleComponent>(args.User, out var buckle) || buckle.BuckledTo != ent.Owner)
+            return;
+
+        _buckle.TryUnbuckle(args.User, args.User, buckle);
     }
 
     private void OnRiderPickupAttempt(Entity<BorgRiderComponent> ent, ref PickupAttemptEvent args)
