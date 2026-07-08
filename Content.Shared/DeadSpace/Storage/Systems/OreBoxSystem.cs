06624edfe7905f5c21cbab2da842a4f984ba56e5
а а <atickov222@gmail.com>
2026-07-08 17:23:43 +0300

    бубубубу

---
+++ Content.Shared/DeadSpace/Storage/Systems/OreBoxSystem.cs
@@ -0,0 +1,64 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
+using Content.Shared.Interaction;
+using Content.Shared.DragDrop;
+using Content.Shared.Materials;
+using Content.Shared.Tag;
+using Content.Shared.Lathe;
+using Robust.Shared.Containers;
+using Content.Shared.DeadSpace.Storage.Components;
+
+namespace Content.Shared.DeadSpace.Storage.Systems;
+
+public sealed class OreBoxSystem : EntitySystem
+{
+    [Dependency] private readonly TagSystem _tag = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<OreBoxComponent, CanDropTargetEvent>(OnCanDropTarget);
+        SubscribeLocalEvent<OreBoxComponent, DragDropTargetEvent>(OnDragDropTarget);
+        SubscribeLocalEvent<OreBoxComponent, CanDragEvent>(OnCanDrag);
+
+        SubscribeLocalEvent<OreBoxComponent, CanDropDraggedEvent>(OnCanDropDragged);
+    }
+
+    private void OnCanDrag(EntityUid uid, OreBoxComponent comp, ref CanDragEvent args)
+    {
+        args.Handled = true;
+    }
+
+    private void OnCanDropDragged(EntityUid uid, OreBoxComponent comp, ref CanDropDraggedEvent args)
+    {
+        if (args.Handled) return;
+
+        if (!HasComp<LatheComponent>(args.Target)) return;
+
+        args.CanDrop = true;
+        args.Handled = true;
+    }
+
+    private void OnCanDropTarget(EntityUid uid, OreBoxComponent comp, ref CanDropTargetEvent args)
+    {
+        if (args.Handled) return;
+        if (!_tag.HasTag(args.Dragged, "Ore") && !HasComp<MaterialComponent>(args.Dragged)) return;
+
+        args.CanDrop = true;
+        args.Handled = true;
+    }
+
+    private void OnDragDropTarget(EntityUid uid, OreBoxComponent comp, ref DragDropTargetEvent args)
+    {
+        if (args.Handled) return;
+        if (!_tag.HasTag(args.Dragged, "Ore") && !HasComp<MaterialComponent>(args.Dragged)) return;
+
+        var container = _container.EnsureContainer<Container>(uid, "storagebase");
+        if (_container.Insert(args.Dragged, container))
+        {
+            args.Handled = true;
+        }
+    }
+}
