f6837546fa981166698b134fb7ff2fb789f2b154
Tea2283 <artembasov148@gmail.com>
2026-07-09 12:44:30 +0500

    ыфвывфыв

--- Content.Server/Clothing/Systems/OutfitSystem.cs
+++ Content.Server/Clothing/Systems/OutfitSystem.cs
@@ -1,8 +1,6 @@
-﻿using Content.Server.Hands.Systems;
-using Content.Server.Preferences.Managers;
+﻿using Content.Server.Preferences.Managers;
 using Content.Shared.Access.Components;
 using Content.Shared.Clothing;
-using Content.Shared.Hands.Components;
 using Content.Shared.Humanoid;
 using Content.Shared.Interaction.Components;
 using Content.Shared.Inventory;
@@ -11,7 +9,6 @@ using Content.Shared.Preferences;
 using Content.Shared.Preferences.Loadouts;
 using Content.Shared.Roles;
 using Content.Shared.Station;
-using Content.Shared.Storage.EntitySystems; // DS14
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 
@@ -21,10 +18,8 @@ public sealed class OutfitSystem : EntitySystem
 {
     [Dependency] private readonly IServerPreferencesManager _preferenceManager = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly HandsSystem _handSystem = default!;
     [Dependency] private readonly InventorySystem _invSystem = default!;
     [Dependency] private readonly SharedStationSpawningSystem _spawningSystem = default!;
-    [Dependency] private readonly SharedStorageSystem _storageSystem = default!; // DS14
 
     public bool SetOutfit(EntityUid target, string gear, Action<EntityUid, EntityUid>? onEquipped = null, bool unremovable = false)
     {
@@ -45,61 +40,32 @@ public sealed class OutfitSystem : EntitySystem
             profile = prefs.SelectedCharacter as HumanoidCharacterProfile;
         }
 
+        // DS14-Start
         if (_invSystem.TryGetSlots(target, out var slots))
         {
             foreach (var slot in slots)
-            {
                 _invSystem.TryUnequip(target, slot.Name, true, true, false, inventoryComponent);
-                var gearStr = ((IEquipmentLoadout) startingGear).GetGear(slot.Name);
-                if (gearStr == string.Empty)
+        }
+        _spawningSystem.EquipStartingGear(target, startingGear, raiseEvent: false);
+
+        if (_invSystem.TryGetSlots(target, out var filledSlots))
+        {
+            foreach (var slot in filledSlots)
+            {
+                if (!_invSystem.TryGetSlotEntity(target, slot.Name, out var slotEntity))
                     continue;
 
-                var equipmentEntity = Spawn(gearStr, Comp<TransformComponent>(target).Coordinates);
                 if (slot.Name == "id" &&
-                    TryComp(equipmentEntity, out PdaComponent? pdaComponent) &&
+                    TryComp(slotEntity.Value, out PdaComponent? pdaComponent) &&
                     TryComp<IdCardComponent>(pdaComponent.ContainedId, out var id))
                 {
                     id.FullName = Comp<MetaDataComponent>(target).EntityName;
                 }
 
-                _invSystem.TryEquip(target, equipmentEntity, slot.Name, silent: true, force: true, inventory: inventoryComponent);
                 if (unremovable)
-                    EnsureComp<UnremoveableComponent>(equipmentEntity);
+                    EnsureComp<UnremoveableComponent>(slotEntity.Value);
 
-                onEquipped?.Invoke(target, equipmentEntity);
-            }
-        }
-
-        if (TryComp(target, out HandsComponent? handsComponent))
-        {
-            var coords = Comp<TransformComponent>(target).Coordinates;
-            foreach (var prototype in startingGear.Inhand)
-            {
-                var inhandEntity = Spawn(prototype, coords);
-                _handSystem.TryPickup(target, inhandEntity, checkActionBlocker: false, handsComp: handsComponent);
-            }
-        }
-
-        // DS14-Start - заполнение сторадж...
-        if (startingGear.Storage.Count > 0)
-        {
-            var coords = Comp<TransformComponent>(target).Coordinates;
-            if (_invSystem.TryGetSlots(target, out var slotDefs))
-            {
-                foreach (var (slotName, entProtos) in startingGear.Storage)
-                {
-                    if (entProtos.Count == 0)
-                        continue;
-
-                    if (_invSystem.TryGetSlotEntity(target, slotName, out var slotEnt))
-                    {
-                        foreach (var entProto in entProtos)
-                        {
-                            var item = Spawn(entProto, coords);
-                            _storageSystem.Insert(slotEnt.Value, item, out _, playSound: false);
-                        }
-                    }
-                }
+                onEquipped?.Invoke(target, slotEntity.Value);
             }
         }
         // DS14-End
