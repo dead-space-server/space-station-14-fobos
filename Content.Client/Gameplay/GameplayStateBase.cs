e5b6870f65a51b1ab00529ee35f37651b5426e3b
Tea2283 <artembasov148@gmail.com>
2026-07-11 22:56:14 +0500

    выфвфыв

--- Content.Client/Gameplay/GameplayStateBase.cs
+++ Content.Client/Gameplay/GameplayStateBase.cs
@@ -242,9 +242,11 @@ namespace Content.Client.Gameplay
                 var transformSystem = _entitySystemManager.GetEntitySystem<SharedTransformSystem>();
                 var mapSystem = _entitySystemManager.GetEntitySystem<MapSystem>();
 
-                coordinates = _mapManager.TryFindGridAt(mousePosWorld, out var uid, out _) ?
-                    mapSystem.MapToGrid(uid, mousePosWorld) :
-                    transformSystem.ToCoordinates(mousePosWorld);
+                coordinates = !_mapManager.MapExists(mousePosWorld.MapId)
+                    ? EntityCoordinates.Invalid
+                    : _mapManager.TryFindGridAt(mousePosWorld, out var uid, out _)
+                        ? mapSystem.MapToGrid(uid, mousePosWorld)
+                        : transformSystem.ToCoordinates(mousePosWorld);
             }
             else
             {
---
+++ Content.Server/DeadSpace/Arena/ToggleArenaCommand.cs
@@ -0,0 +1,18 @@
+using Content.Server.Administration;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server.DeadSpace.Arena;
+
+[AdminCommand(AdminFlags.Fun)]
+public sealed class ToggleArenaCommand : LocalizedCommands
+{
+    public override string Command => "togglearena";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var arenaSystem = EntitySystem.Get<ArenaSystem>();
+        arenaSystem.ToggleEnabled();
+        shell.WriteLine($"Arena is now {(arenaSystem.Enabled ? "enabled" : "disabled")}");
+    }
+}
