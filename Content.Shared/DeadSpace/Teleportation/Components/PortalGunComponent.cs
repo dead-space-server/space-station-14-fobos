ea3d8276f587bd1724d55042f6be8bbe21586d78
RIP6100 <saulasereza419@gmail.com>
2026-07-31 02:00:29 +0900

    i forgor

--- Content.Shared/DeadSpace/Teleportation/Components/PortalGunComponent.cs
+++ Content.Shared/DeadSpace/Teleportation/Components/PortalGunComponent.cs
@@ -1,5 +1,7 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.DeadSpace.Teleportation.Components;
 
 [RegisterComponent, NetworkedComponent]
