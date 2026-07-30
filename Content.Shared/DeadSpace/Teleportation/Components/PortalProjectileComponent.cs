5fdf24a3460db34dde4911c351eceea66a6efc5e
RIP6100 <saulasereza419@gmail.com>
2026-07-31 01:44:41 +0900

    A LOT OF STUFF

---
+++ Content.Shared/DeadSpace/Teleportation/Components/PortalProjectileComponent.cs
@@ -0,0 +1,22 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.DeadSpace.Teleportation.Components;
+
+[RegisterComponent]
+public sealed partial class PortalProjectileComponent : Component
+{
+    [ViewVariables]
+    public EntityUid? Destination;
+
+    [DataField]
+    public EntProtoId NearPortal = "PortalBlue";
+
+
+    [DataField]
+    public EntProtoId FarPortal = "PortalRed";
+
+    [DataField]
+    public TimeSpan Lifetime = TimeSpan.FromSeconds(10);
+}
