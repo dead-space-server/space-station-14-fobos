88eef60423bb11d823543f71240e0927ba7f0f42
Алексей Герасимов <gerasimovav@althub.pro>
2026-08-01 17:55:17 +0300

    fix: make multi-clothing equipment transactional

--- Content.Shared/DeadSpace/Clothing/MultiClothingComponent.cs
+++ Content.Shared/DeadSpace/Clothing/MultiClothingComponent.cs
@@ -1,5 +1,7 @@
-using Robust.Shared.Prototypes;
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.DeadSpace.Clothing;
 
