cc7bb3865ce2bd2ef30a8e4069fcc59ec78bf8b7
MTF97 <yarema.suhotskiy@gmail.com>
2026-08-01 13:06:24 +0300

    fix

--- Content.Server/Mobs/DeathgaspSystem.cs
+++ Content.Server/Mobs/DeathgaspSystem.cs
@@ -1,31 +1,28 @@
-using Content.Server.Chat.Systems;
+﻿using Content.Server.Chat.Systems;
 using Content.Server.Speech.Muting;
-using Content.Shared.Inventory;
+using Content.Shared.Inventory; // DS14
 using Content.Shared.Mobs;
 using Content.Shared.Speech.Muting;
-using Robust.Shared.Audio.Systems;
+using Robust.Shared.Audio.Systems; // DS14
 using Robust.Shared.Prototypes;
 
 namespace Content.Server.Mobs;
 
 /// <see cref="DeathgaspComponent"/>
+public sealed class DeathgaspSystem: EntitySystem
 public sealed class DeathgaspSystem : EntitySystem
 {
     [Dependency] private readonly ChatSystem _chat = default!;
-    [Dependency] private readonly InventorySystem _inventory = default!;
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!; // DS14
+    [Dependency] private readonly SharedAudioSystem _audio = default!; // DS14
 
     public override void Initialize()
     {
-        base.Initialize();
-
-        SubscribeLocalEvent<DeathgaspComponent, MobStateChangedEvent>(OnMobStateChanged);
-    }
-
     private void OnMobStateChanged(EntityUid uid, DeathgaspComponent component, MobStateChangedEvent args)
     {
         // don't deathgasp if they arent going straight from crit to dead
-        if (args.NewMobState != MobState.Dead ||
+        if (args.NewMobState != MobState.Dead  args.OldMobState is not (MobState.Critical or MobState.PreCritical)) // DS14 edited
+        if (args.NewMobState != MobState.Dead
             args.OldMobState is not (MobState.Critical or MobState.PreCritical))
             return;
 
@@ -38,23 +35,10 @@ public sealed class DeathgaspSystem : EntitySystem
             Deathgasp(uid, component);
             return;
         }
-
+     // DS14-end
         Deathgasp(uid, component);
     }
-    // DS14-end
     /// <summary>
     ///     Causes an entity to perform their deathgasp emote, if they have one.
     /// </summary>
-    public bool Deathgasp(EntityUid uid, DeathgaspComponent? component = null)
-    {
-        if (!Resolve(uid, ref component, false))
-            return false;
-
-        if (HasComp<MutedComponent>(uid))
-            return false;
-
-        _chat.TryEmoteWithChat(uid, component.Prototype, ignoreActionBlocker: true);
-
-        return true;
-    }
-}
+    ///
