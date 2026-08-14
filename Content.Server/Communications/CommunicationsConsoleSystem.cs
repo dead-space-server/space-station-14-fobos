9157a2d1897f6cdf805f62247f6e4acc57d66580
а а <atickov222@gmail.com>
2026-08-14 19:09:44 +0300

    оаыоыаоыа

--- Content.Server/Communications/CommunicationsConsoleSystem.cs
+++ Content.Server/Communications/CommunicationsConsoleSystem.cs
@@ -30,7 +30,7 @@ using Robust.Shared.Configuration;
 using Content.Server.DeadSpace.Languages;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
-using Content.Server.GameTicking; //DS14
+using Content.Shared.GameTicking; //DS14
 
 namespace Content.Server.Communications
 {
@@ -77,7 +77,7 @@ namespace Content.Server.Communications
             SubscribeLocalEvent<CommunicationsConsoleComponent, BoundUIOpenedEvent>(OnBoundUiOpened);
             SubscribeLocalEvent<CommunicationsConsoleComponent, BoundUIClosedEvent>(OnBoundUiClosed);
 
-SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRestartRound);
+            SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRestartRound);
             // DS14-end
 
             // On console init, set cooldown
@@ -396,10 +396,10 @@ SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRestartRound);
             _adminLogger.Add(LogType.Action, LogImpact.High, $"{ToPrettyString(message.Actor):player} has recalled the shuttle.");
         }
         // DS14-start
-        private void   OnRestartRound(RoundRestartCleanupEvent ev)
-{
-    _cfg.SetCVar(CCVars.EvacLocked, false);
-}
+        private void OnRestartRound(RoundRestartCleanupEvent ev)
+        {
+            _cfg.SetCVar(CCVars.EvacLocked, false);
+        }
 
         private void OnBoundUiOpened(EntityUid uid, CommunicationsConsoleComponent component, BoundUIOpenedEvent args)
         {
