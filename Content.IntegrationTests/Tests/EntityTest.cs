98366042dafbb165a0d5879b91a29273cc9b1f7e
Алексей Герасимов <gerasimovav@althub.pro>
2026-08-21 19:08:30 +0300

    fix

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -183,7 +183,7 @@ namespace Content.IntegrationTests.Tests
                 }
             });
 
-            await pair.RunTicksSync(15);
+            await pair.RunUntilSynced(); // DS14
 
             // Make sure the client actually received the entities
             // 500 is completely arbitrary. Note that the client & sever entity counts aren't expected to match.
