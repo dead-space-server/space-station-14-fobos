c9c9045abd646dd6e560fa1111a5d6558a3cb979
Whatstone <166147148+whatston3@users.noreply.github.com>
2026-08-24 11:37:33 +0300

    feat: expose APC power receiver fields to prototypes (#45022)

--- Content.Shared/Power/Components/SharedApcPowerReceiverComponent.cs
+++ Content.Shared/Power/Components/SharedApcPowerReceiverComponent.cs
@@ -5,21 +5,27 @@ namespace Content.Shared.Power.Components;
 [NetworkedComponent]
 public abstract partial class SharedApcPowerReceiverComponent : Component
 {
+    /// <summary>
+    /// If true, this entity either doesn't need power, or is currently receiving the power it needs.
+    /// </summary>
     [ViewVariables]
     public bool Powered;
 
     /// <summary>
-    ///     When false, causes this to appear powered even if not receiving power from an Apc.
+    /// When false, causes this to appear powered even if not receiving power from an Apc.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    public virtual bool NeedsPower { get; set;}
+    [DataField]
+    public virtual bool NeedsPower { get; set; } = true;
 
     /// <summary>
-    ///     When true, causes this to never appear powered.
+    /// When true, causes this to never appear powered.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public virtual bool PowerDisabled { get; set; }
 
-    // Doesn't actually do anything on the client just here for shared code.
-    public abstract float Load { get; set; }
+    /// <summary>
+    /// Amount of power this needs from an APC to function, in watts.
+    /// </summary>
+    [DataField("powerLoad")]
+    public virtual float Load { get; set; }
 }
