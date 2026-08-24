165fe5b80bb13c7237d0d6123172dd6a1dc5bac4
Samuka <47865393+Samuka-C@users.noreply.github.com>
2026-08-24 11:42:10 +0300

    feat: add xenoborg shock module (#44391)

--- Content.Shared/Electrocution/SharedElectrocutionSystem.cs
+++ Content.Shared/Electrocution/SharedElectrocutionSystem.cs
@@ -1,11 +1,13 @@
 using Content.Shared.Inventory;
 using Content.Shared.StatusEffect;
+using Robust.Shared.Timing;
 
 namespace Content.Shared.Electrocution
 {
     public abstract class SharedElectrocutionSystem : EntitySystem
     {
         [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+        [Dependency] private readonly IGameTiming _timing = default!; // DS14 - pre-v288 IoC
 
         public override void Initialize()
         {
@@ -14,6 +16,8 @@ namespace Content.Shared.Electrocution
             SubscribeLocalEvent<InsulatedComponent, ElectrocutionAttemptEvent>(OnInsulatedElectrocutionAttempt);
             // as long as legally distinct electric-mice are never added, this should be fine (otherwise a mouse-hat will transfer it's power to the wearer).
             SubscribeLocalEvent<InsulatedComponent, InventoryRelayedEvent<ElectrocutionAttemptEvent>>((e, c, ev) => OnInsulatedElectrocutionAttempt(e, c, ev.Args));
+
+            SubscribeLocalEvent<ElectrifiedComponent, MapInitEvent>(OnInit);
         }
 
         public void SetInsulatedSiemensCoefficient(EntityUid uid, float siemensCoefficient, InsulatedComponent? insulated = null)
@@ -76,6 +80,12 @@ namespace Content.Shared.Electrocution
                 return;
 
             args.SiemensCoefficient *= insulated.Coefficient;
+        }
+
+        private void OnInit(Entity<ElectrifiedComponent> entity, ref MapInitEvent args)
+        {
+            entity.Comp.NextShock = _timing.CurTime;
+            Dirty(entity);
         }
     }
 }
